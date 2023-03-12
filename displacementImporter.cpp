8b8b4f117345c53ecee1403cac10f18b9a2b33df
minoue <michitaka.inoue@icloud.com>
2023-03-12 19:43:29 +0000

    Use zbrush internal variables for program path and obj file path

--- displacementImporter.cpp
+++ displacementImporter.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <filesystem>
 
 #include "displacementImporter.hpp"
 
@@ -17,73 +18,77 @@ float EXPORT ImportVectorDisplacement(char* textFromZBrush,
                                       int optBuffer2Size,
                                       char** zData)
 {
-
-    std::string logPath;
-
-#ifdef _WIN32
-    logPath = "C:/Users/Public/Pixologic/displacementImporter.log";
-#else // MacOS
-    logPath = "/tmp/displacementImporter.log";
-#endif
-    
-    // log
-    std::ofstream logOfs(logPath, std::ios::out);
-    if (!logOfs) {
-        strcpy(pOptBuffer2, "Failed to open the log file.");
-        return 1.0;
-    } 
-
-    int zbrushVersion = static_cast<int>(zversion);
-    std::string objFile(textFromZBrush);    // from zbrush
-    std::vector<std::string> texture_paths;
+    std::vector<std::string> cmdVector;
     std::string cmdString(pOptBuffer1);
+
     int cmdLength = static_cast<int>(cmdString.length());
 
+    // Split/Convert command strings
     std::string path;
-
     for (int i = 0; i < cmdLength; i++) {
         const char c = pOptBuffer1[i];
         if (c == '#') {
-            texture_paths.push_back(path);
+            cmdVector.push_back(path);
             path.clear();
         } else {
             path.push_back(c);
         }
     }
 
-    std::stringstream cmdstream;
+    // Init paths
+    std::string objModifierPath = cmdVector[0];
+    std::string objFilePath = cmdVector[1];
+
+    objModifierPath.erase(0, 2);
+    objFilePath.erase(0, 2);
 
 #ifdef _WIN32
-    cmdstream << "\"C:/Program Files/Pixologic/ZBrush 2022/ZStartup/ZPlugs64/displacementImporter/objModifier.exe\"";
+    objModifierPath.insert(0, "\"C:");
+    objFilePath.insert(0, "C:");
 #else // MacOS
-    cmdstream << "/Applications/ZBrush\\ " << zbrushVersion << "/ZStartup/ZPlugs64/displacementImporter/objModifier";
+    objModifierPath.insert(0, "\"");
 #endif
+    objModifierPath.append("\"");
 
-    cmdstream << " -i " << objFile << " -o ZBrush_displacementImporter_in" << " -t"; 
+    std::vector<std::string> texture_paths(cmdVector.begin() + 2, cmdVector.end());
+
+    // log path
+    std::filesystem::path cmdLog = objFilePath;
+    cmdLog.replace_filename("dspImporter.log");
+    std::filesystem::path pluginLog = objFilePath;
+    pluginLog.replace_filename("objModifier.log");
+
+    std::ofstream logOfs(cmdLog, std::ios::out);
+    if (!logOfs) {
+        strcpy(pOptBuffer2, "Failed to open the log file.");
+        return 1.0;
+    } 
+
+    // Run command setup
+    std::stringstream cmdstream;
+    cmdstream << objModifierPath << " -i " << objFilePath << " -o dspImporter_from_DLL -t";
     for (std::string& tex : texture_paths) {
         cmdstream << " " << tex;
     }
-    // cmdstream << " > " << logPath;
-
+#ifdef __APPLE__
+    cmdstream << " > " << pluginLog;
+#endif
+    
+    logOfs << cmdstream.str() << std::endl;
+    logOfs.close();
+    
     auto ret = system(nullptr);
-    if (ret != 0) {
-        std::cout << "shell available" << std::endl;
-        int r = system(cmdstream.str().c_str());
-        if (r != 0) {
-            // not successful
-            strcpy(pOptBuffer2, "Failed to run the plugin command. Check the log and send it to developer.");
-            return 1.0;
-        }
-    } else {
-        std::cout << "shell is NOT available" << std::endl;
+    if (ret == 0) {
+        strcpy(pOptBuffer2, "Shell not available");
         return 1.0;
     }
 
-    logOfs << "Command running: " << std::endl;
-    logOfs << "ZBrush version : " << zbrushVersion << std::endl;
-    logOfs << cmdstream.str() << std::endl;
-    logOfs << "Done" << std::endl;
-    logOfs.close();
+    int r = system(cmdstream.str().c_str());
+    if (r != 0) {
+        // not successful
+        strcpy(pOptBuffer2, "Failed to run the plugin command. Check the log.");
+        return 1.0;
+    }
 
     return 0.0f;
 }
